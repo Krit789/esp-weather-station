2c645d1abb8e86fcb3aea093a6be3224e200bfc1
Jarukrit <jarukritsripaploen@protonmail.com>
2023-10-17 18:26:42 +0700

    Added Data API

---
+++ webserver/server/api/sensor.get.ts
@@ -0,0 +1,18 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DateTime } from "luxon";
+
+export default defineEventHandler(async () => {
+  const prisma = new PrismaClient();
+  const sensor_data = await prisma.sensor_data.findMany({
+    where: {
+      time: {
+        gte: DateTime.now().minus({ hours: 6 }).toJSDate(),
+        lte: DateTime.now().toJSDate(),
+      },
+    },
+  });
+  return {
+    status: 1,
+    message: sensor_data,
+  };
+});
--- webserver/server/api/sensor.post.ts
+++ webserver/server/api/sensor.post.ts
@@ -3,7 +3,16 @@ import { Prisma, PrismaClient } from "@prisma/client";
 export default defineEventHandler(async (event) => {
   const prisma = new PrismaClient();
   let postBody = await readBody(event);
-  let sensor_entry: Prisma.sensor_dataCreateInput
-  sensor_entry = postBody;
+  let sensor_entry: Prisma.sensor_dataCreateInput = postBody
   const sensor_data = await prisma.sensor_data.create({data: sensor_entry})
+  if (sensor_data) {
+    return {
+      status: 1,
+      message: "Recorded data successfully"
+    }
+  }
+  return {
+    status: 0,
+    message: "Failed to record data"
+  }
 });
