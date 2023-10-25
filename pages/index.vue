de392adbbf74d752ca6fea7a4d784daafee97e3e
bess11234 <combit@kmitl.ac.th>
2023-10-25 16:02:57 +0700

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@ import '~/assets/stylesheets/global.css';
 import '~/assets/stylesheets/index.css';
 </script>
 
-
 <template>
-  <div class="ma-10 text-center" style="margin-bottom: 50px">
+  <div class="ma-10 text-center" style="margin-bottom: 50px;margin-top:20px">
     <h1 class="text-5xl font-bold">The Forecast Station</h1>
+    <img class="img" width="25%" src="../asset_img/rainbow.svg">
     <p>Predicting weather from surrounding readings from sensors</p>
   </div>
   <div class="columns-2 text-center">
@@ -19,10 +19,25 @@ import '~/assets/stylesheets/index.css';
       <line x1="0" y1="0" x2="25000" y2="0" style="stroke:rgb(0,0,0);stroke-width:5px"/>
     </svg>
   </div>
-  <div class="columns-4 text-center">
-    <img src="../asset_img/GY-BMP280.svg">
-    <img>
-    <img>
+  <div class="grid grid-cols-3 text-center" style="align-content: center;">
+    <div class="invisible" style="margin: auto"><span class="text-2xl">LDR Light Sensor</span><p class="text-sm">Photoresistor as a light detector</p><img class="img" src="../asset_img/Light_Sensor.svg" width="100%"></div>
+    <div style="margin: auto">
+      <div><span class="text-2xl">NodeMCU ESP8266</span><p class="text-sm">NodeMCU V3 LUA based ESP8266-12E for wifi connection with a leg extender board</p></div>
+      <div class="columns-2"><img class="img" src="../asset_img/NodeMCU%20ESP8266-1.svg" width="100%"><img class="img" src="../asset_img/NodeMCU%20ESP8266-2.svg" width="70%"></div>
+    </div>
+    <div class="invisible" style="margin: auto"><span class="text-2xl">GY-BMP280</span><p class="text-sm">High precision Atmospheric Pressure Sensor Module</p><img class="img" src="../asset_img/GY-BMP280.svg" width="96%"></div>
+  </div>
+  <div class="grid grid-cols-3 text-center" style="align-content: center;">
+    <div style="margin: auto"><span class="text-2xl">GY-BMP280</span><p class="text-sm">High precision Atmospheric Pressure Sensor Module</p><img class="img" src="../asset_img/GY-BMP280.svg" width="96%"></div>
+    <div style="margin: auto">
+      <div><span class="text-2xl">LCD module</span><p class="text-sm">Liquid crystal display as another output </p></div><img class="img" src="../asset_img/LCD%20module.svg" width="70%">
+    </div>
+    <div style="margin: auto"><span class="text-2xl">LDR Light Sensor</span><p class="text-sm">Photoresistor as a light detector</p><img class="img" src="../asset_img/Light_Sensor.svg" width="100%"></div>
+  </div>
+  <div>
+    <svg width="25000" height="5">
+      <line x1="0" y1="0" x2="25000" y2="0" style="stroke:rgb(0,0,0);stroke-width:5px"/>
+    </svg>
   </div>
 </template>
 <script setup lang="ts">
