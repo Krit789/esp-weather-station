f0a4cc340a7dd324e1009b320685a829f50c7e05
tae13152 <tanakritsupprasit@gmail.com>
2023-10-25 14:47:35 +0700

    Added stylesheets

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
+<script lang="ts" setup>
+import '~/assets/stylesheets/global.css';
+import '~/assets/stylesheets/index.css';
+</script>
+
+
 <template>
-    <div class="ma-10 text-center" style="margin-bottom: 50px">
+  <div class="ma-10 text-center" style="margin-bottom: 50px">
     <h1 class="text-5xl font-bold">The Forecast Station</h1>
     <p>Predicting weather from surrounding readings from sensors</p>
-    </div>
-    <div class="columns-4 md:text-center">
-      <p>asd;asp[d</p>
-      <p>asd;asp[d</p>
-      <p>asd;asp[d</p>
-      <p>asd;asp[d</p>
-    </div>
+  </div>
+  <div class="columns-4 md:text-center">
+    <p>asd;asp[d</p>
+    <p>asd;asp[d</p>
+    <p>asd;asp[d</p>
+    <p>asd;asp[d</p>
+  </div>
 </template>
-<script setup lang="ts">
+<script lang="ts" setup>
 </script>
