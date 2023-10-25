137b550e3ae701576742355a82fa54f0fa0ba01a
tae13152 <tanakritsupprasit@gmail.com>
2023-10-25 19:01:38 +0700

    Revert "Updated Background"
    This reverts commit 4e887a9e

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    modules: ["@nuxtjs/tailwindcss", '@nuxt/image'],
+    modules: ["@nuxtjs/tailwindcss"],
     app: {
         baseURL: '/esp-weather-station/', // baseURL: '/<repository>/'
         buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
-    },
-    image: {
-        dir: 'assets/images',
-        format: ['avif', 'webp', 'png'],
-        quality: 90
-    },
+    }
 })
